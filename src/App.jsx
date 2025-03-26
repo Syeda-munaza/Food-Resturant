bf477b0b5ad272e2872148733d2586fbe7c8bef0
Syeda_munaza <imunazaizhar@gmail.com>
2025-03-26 16:35:35 +0500

    food-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import "./App.css";
 import React from "react";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Menu from "./components/Menu";
+import Product from "./components/Product";
+import Review from "./components/Review";
+import ContactUs from "./components/ContactUs";
+import Blogs from "./components/Blogs";
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
      <Navbar/>
+     <Home />
+     <About/>
+     <Menu/>
+     <Product/>
+     <Review/>
+     <ContactUs/>
+     <Blogs/>
+     <Footer/>
     </>
   );
 }
